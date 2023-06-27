94067cf5792581ae37f0a374b31c275bc6702e36
Dakshina <dakshinard3010@gmail.com>
2023-06-27 20:38:57 +0530

    variadic parameter

---
+++ Functions_GO/practice13.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+	xi:=[] int {1,2,3,4,5,6,7,8,9}
+	fmt.Println(foo(xi...))
+}
+func foo(ii...int)int{
+	t :=0
+	for _,v:= range ii{
+		t += v
+	}
+	return t
+}
